e910c37d86baff6910a8980315805ac613b3b637
Jonathan Lofgren <jonathan.lofgren@quantumblack.com>
2019-10-23 15:34:20 +0800

    Fix for python 3.6+ and pandas 0.25+

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='spark-df-profiling',
-    version='1.1.13',
+    version='1.2.0',
     author='Julio Antonio Soto de Vicente',
     author_email='julio@esbet.es',
     packages=['spark_df_profiling'],
@@ -17,12 +17,12 @@ setup(
     license='MIT',
     description='Create HTML profiling reports from Apache Spark DataFrames',
     install_requires=[
-        "pandas>=0.17.0",
+        "pandas>=0.25",
         "matplotlib>=1.4",
         "jinja2>=2.8",
         "six>=1.9.0"
     ],
-    include_package_data = True,
+    include_package_data=True,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Topic :: Software Development :: Build Tools',
@@ -33,12 +33,8 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Scientific/Engineering',
         'Framework :: IPython',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
-
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7'
     ],
     keywords='spark pyspark report big-data pandas data-science data-analysis python jupyter ipython',
 
